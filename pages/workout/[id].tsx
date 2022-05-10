d4f9c5af0205d78d0a3076b65e50870ac39a2af5
Sebastian <sebastian17pepp@gmail.com>
2022-05-10 16:01:58 +0200

    Added proper eslint

--- pages/workout/[id].tsx
+++ pages/workout/[id].tsx
@@ -1,43 +1,42 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { groq } from "next-sanity";
 import { useRouter } from "next/router";
-import React from "react";
-import { GetUrlId } from "../../lib/helpers";
+import { NextResponse } from "next/server";
+import React, { useMemo } from "react";
 import { Workout } from "../../types/SchemaTypes";
 import { client } from "../api/client";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  if (!params) return { props: {} };
-  const p = params.id as string;
-  const workout: Workout[] = await client.fetch(
-    groq`*[_type=="workout" && references(*[_type=="workoutday" && _id == $p]._id)]
+	if (!params) return { props: {} };
+	const p = params.id as string;
+	const workout: Workout[] = await client.fetch(
+		groq`*[_type=="workout" && references(*[_type=="workoutday" && _id == $p]._id)]
     {
       workoutday,
       _id,
       workoutweekref
     }`
-  );
-  console.log(workout);
-  return {
-    props: {
-      workout,
-    },
-  };
+	);
+	console.log(workout);
+	return {
+		props: {
+			workout,
+		},
+	};
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const queryId = useRouter().query.id;
-  const paths = [
-    {
-      params: {
-        id: queryId,
-      },
-    },
-  ];
-  return {
-    paths,
-    fallback: false,
-  };
+	const paths = [
+		{
+			params: {
+				id: "queryId",
+			},
+		},
+	];
+	return {
+		paths,
+		fallback: false,
+	};
 };
 
 interface DayProps {
@@ -45,15 +44,36 @@ interface DayProps {
 }
 
 const Day: React.FC<DayProps> = ({ workouts }) => {
-  return (
-    <>
-      {workouts.length > 0 && (
-        <div>
-          <pre>{JSON.stringify(workouts, null, 2)}</pre>
-        </div>
-      )}
-    </>
-  );
+	const router = useRouter();
+
+	const workoutData = useMemo(() => {
+		client
+			.fetch(
+				groq`*[_type=="workout" && references(*[_type=="workoutday" && _id == $p]._id)]
+      {
+        workoutday,
+        _id,
+        workoutweekref
+      }`
+			)
+			.then((response: NextResponse) => {
+				console.log(response);
+				if (response.status === 200) {
+					return response.body;
+				}
+				return [];
+			}).catch(() => []);
+	}, []);
+
+	return (
+		<>
+			{workouts.length > 0 && (
+				<div>
+					<pre>{JSON.stringify(workouts, null, 2)}</pre>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Day;

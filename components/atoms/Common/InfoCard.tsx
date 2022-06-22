d80214bba16fd4dc357ef480dabc572fcd16ab68
Sebastian <sebastian17pepp@gmail.com>
2022-06-22 13:05:36 +0200

    Added InfoCard for report findings

---
+++ components/atoms/Common/InfoCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card from '../Card';
+import { StyledSvg } from '../StyledComponents/StyledSvg';
+
+interface InfoCardProps {
+	redirectLink: string;
+	redirectText: string;
+	title: string;
+	imgSrc?: string;
+}
+const InfoCard: React.FC<InfoCardProps> = ({
+	title,
+	imgSrc = '/piggy-bank.png',
+	redirectLink,
+	redirectText,
+}) => {
+	return (
+		<Wrapper>
+			<>
+				<div id="firstelement">
+					<StyledSvg src={imgSrc} alt="logo" />
+					<p>{title}</p>
+				</div>
+				<a href={redirectLink}>{redirectText}</a>
+			</>
+		</Wrapper>
+	);
+};
+
+export default InfoCard;
+
+const Wrapper = styled(Card)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #e6f1f5;
+	padding: 1em 1.5em;
+	p {
+		margin: 0;
+	}
+	#firstelement {
+		display: inherit;
+		column-gap: 1em;
+		align-items: inherit;
+		img {
+			width: 2em;
+			height: 2em;
+		}
+	}
+`;
--- pages/tsinsp/testarea.tsx
+++ pages/tsinsp/testarea.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import InfoCard from '../../components/atoms/Common/InfoCard';
 import LinkCard from '../../components/atoms/Common/LinkCard';
 import { VeivesenRoutes } from '../../Constants/routes';
 
@@ -11,6 +12,11 @@ const TestArea: React.FC = () => {
 				subTitleText="Vanlig TS-Inspeksjon"
 				url={VeivesenRoutes.overviewLayout}
 			/>
+			<InfoCard
+				title="Dette funnet har status “uten finansiering”."
+				redirectText="Se oppfølgingsside"
+				redirectLink={VeivesenRoutes.overviewLayout}
+			/>
 		</Wrapper>
 	);
 };
@@ -21,4 +27,7 @@ const Wrapper = styled.div`
 	width: 80%;
 	margin: auto;
 	padding-top: 5em;
+	display: flex;
+	flex-direction: column;
+	row-gap: 2em;
 `;
